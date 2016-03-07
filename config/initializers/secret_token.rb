d1d659ded0c2e90c9490cbf0fc789104831d6be5
vagrant <chronomaly@gmail.comgit config --global user.name GhostlyMire>
2016-03-07 16:45:05 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grammable::Application.config.secret_key_base = 'dbbca8440f4c889034a7b297e386c55943c32c5ff3f74cdb5af14595005b62e578374a48ad6ca1971ecf396ecd78f40c4106a9cdee267b1892ffbc72c4b6e535'
